7f291cd19db0b1ca51e2dd755275fb980c800943
Oleksandr Shyshatskyi <oshyshatskyi@cloudlinux.com>
2020-07-30 21:44:40 +0300

    fix permissions

--- render/renderd/run.sh
+++ render/renderd/run.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
+if [[ "$1" == "" ]]; then
+  MAPNIK_XML_PATH="/openstreetmap-carto/mapnik.xml"
+  IMPORT_DONE_FILE="/var/run/renderd/mod_tile/.import_done1"
 
-MAPNIK_XML_PATH="/openstreetmap-carto/mapnik.xml"
-IMPORT_DONE_FILE="/var/run/renderd/mod_tile/.import_done1"
+  if [[ ! -f /var/run/renderd/mod_tile/ ]]; then
+    mkdir -p /var/run/renderd/mod_tile/
+  fi
 
-if [[ ! -f /var/run/renderd/mod_tile/ ]]; then
-  mkdir -p /var/run/renderd/mod_tile/
+  # replace config stubs with real values
+  sed -i "s/\${PG_HOSTNAME}/${POSTGRES_HOST}/g" "${MAPNIK_XML_PATH}"
+  sed -i "s/\${PG_DBNAME}/${POSTGRES_DB}/g" "${MAPNIK_XML_PATH}"
+  sed -i "s/\${PG_USERNAME}/${POSTGRES_USER}/g" "${MAPNIK_XML_PATH}"
+  sed -i "s/\${PG_PASSWORD}/${POSTGRES_PASSWORD}/g" "${MAPNIK_XML_PATH}"
+
+  cd /mod_tile && renderd -f -c "${RENDERD_CONF_PATH}"
+else
+  $@
 fi
-
-# replace config stubs with real values
-sed -i "s/\${PG_HOSTNAME}/${POSTGRES_HOST}/g" "${MAPNIK_XML_PATH}"
-sed -i "s/\${PG_DBNAME}/${POSTGRES_DB}/g" "${MAPNIK_XML_PATH}"
-sed -i "s/\${PG_USERNAME}/${POSTGRES_USER}/g" "${MAPNIK_XML_PATH}"
-sed -i "s/\${PG_PASSWORD}/${POSTGRES_PASSWORD}/g" "${MAPNIK_XML_PATH}"
-
-cd /mod_tile && renderd -f -c "${RENDERD_CONF_PATH}"
